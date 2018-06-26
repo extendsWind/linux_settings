5a0ecd6cceb24a175426d492708f2e4a7e7bb191
extendsWind <extendswind@foxmail.com>
2018-06-26 20:16:49 +0800

    change spacemacs backup

--- setDotFile.sh
+++ setDotFile.sh
@@ -18,64 +18,65 @@ $myHome/.config/awesome
 .
 $myHome/.zshrc
 .
-$myHome/.spacemacs.d/init.el
-./spacemacs
+$myHome/.spacemacs.d
+.
 $myHome/.vimrc.local
 ./vim_spf13
 )
 
 
-mklink(){
-	fileNum=${#fileLists[@]}
-	echo total file number: $(($fileNum/2))
-	for ((i=0; i<$fileNum; i++))
-		{
-			if [ "$(($i%2))" = "0" ]; then
-
-                # mkdir for current directory
-                if [ ! -d ${fileLists[i+1]} ]; then
-                    mkdir ${fileLists[i+1]}
-                fi
-
-                # the linked file will be put in fileLists[i+1]
-				lnFile=${fileLists[i+1]}/${fileLists[i]##*/}
-				if [ -f $lnFile ] || [ -d $lnFile ]  ; then
-					rm $lnFile
-					echo $lnFile exist, delete it
-				fi
-
-                # create link (file non-exist is not considerred)
-				ln -s ${fileLists[i]} $lnFile
-				echo create link from ${fileLists[i]} to $lnFile
-			fi
-        }
-
-}
-
+# mklink(){
+# 	fileNum=${#fileLists[@]}
+# 	echo total file number: $(($fileNum/2))
+# 	for ((i=0; i<$fileNum; i++))
+# 		{
+# 			if [ "$(($i%2))" = "0" ]; then
+# 
+#                 # mkdir for current directory
+#                 if [ ! -d ${fileLists[i+1]} ]; then
+#                     mkdir ${fileLists[i+1]}
+#                 fi
+# 
+#                 # the linked file will be put in fileLists[i+1]
+# 				lnFile=${fileLists[i+1]}/${fileLists[i]##*/}
+# 				if [ -f $lnFile ] || [ -d $lnFile ]  ; then
+# 					rm $lnFile
+# 					echo $lnFile exist, delete it
+# 				fi
+# 
+#                 # create link (file non-exist is not considerred)
+# 				ln -s ${fileLists[i]} $lnFile
+# 				echo create link from ${fileLists[i]} to $lnFile
+# 			fi
+#         }
+# 
+# }
+# 
 getConfigFiles(){
 	fileNum=${#fileLists[@]}
 	echo total file number: $(($fileNum/2))
 	for ((i=0; i<$fileNum; i++))
-		{
-			if [ "$(($i%2))" = "0" ]; then
+	{
+		if [ "$(($i%2))" = "0" ]; then
 
-                # mkdir for current directory
-                if [ ! -d ${fileLists[i+1]} ]; then
-                    mkdir ${fileLists[i+1]}
-                fi
+            # mkdir for current directory
+            if [ ! -d ${fileLists[i+1]} ]; then
+                mkdir ${fileLists[i+1]}
+            fi
 
-                # the backup file will be put in ./$fileLists[i+1]
-				bakFile=${fileLists[i+1]}/${fileLists[i]##*/}
-				if [ -f $bakFile ] || [ -d $bakFile ]  ; then
-					rm $bakFile -rf
-					echo $bakFile exist, delete it
-				fi
-
-                # create link (file non-exist is not considerred)
-				cp -r ${fileLists[i]} $bakFile
-				echo cp from ${fileLists[i]} to $bakFile
+            # the backup file will be put in ./$fileLists[i+1]
+			bakFile=${fileLists[i+1]}/${fileLists[i]##*/}
+			if [ -f $bakFile ] || [ -d $bakFile ]  ; then
+				rm $bakFile -rf
+				echo $bakFile exist, delete it
 			fi
-        }
+            echo $bakFile
+
+            # create link (file non-exist is not considerred)
+			cp -r ${fileLists[i]} $bakFile
+			echo cp from ${fileLists[i]} to $bakFile
+		fi
+    }
 }
 
 deploy_settings(){
@@ -104,9 +105,9 @@ deploy_settings(){
 
 }
 
-# getConfigFiles
+getConfigFiles
 
-deploy_settings
+# deploy_settings
 #mklink
 
 # installSoftware  # not test
